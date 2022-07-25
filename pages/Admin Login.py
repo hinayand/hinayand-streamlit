c02c1ec21df64380a30ecbc3426bfa80a9009868
hinayand <3093812306@qq.com>
2022-07-25 17:12:10 +0800

    fix(InputTools):fix some bugs

--- pages/Admin Login.py
+++ pages/Admin Login.py
@@ -3,6 +3,23 @@ from tools.StreamlitTools.InputTools import InputTools
 from tools.PyTools.UserManageTools import UserManager
 import streamlit as st
 
+
+def login(username, password)
+    if umt_object.check_user(username, password):
+        st.success('Login Success')
+        st.sidebar.markdown('[Shell](./admin/Shell)')
+        st.session_state.clear()
+        st.session_state['LoginStatus'] = True
+    else:
+        if umt_object.users == {}:
+            umt_object.add_user(username, password)
+            umt_object.save_users()
+            st.success('Login Success')
+            st.sidebar.markdown('[Shell](./admin/Shell)')
+        else:
+            st.error('Login Failed')
+
+
 umt_object = UserManager(path='./users.json')
 umt_object.load_users()
 
@@ -13,17 +30,4 @@ PageTools().load_markdown_file('./markdown/admin_login.md')
 
 username = InputTools().input_text('Username', '')
 password = InputTools().input_password('Password', '')
-
-if umt_object.check_user(username, password):
-    st.success('Login Success')
-    st.sidebar.markdown('[Shell](./admin/Shell)')
-    st.session_state.clear()
-    st.session_state['LoginStatus'] = True
-else:
-    if umt_object.users == {}:
-        umt_object.add_user(username, password)
-        umt_object.save_users()
-        st.success('Login Success')
-        st.sidebar.markdown('[Shell](./admin/Shell)')
-    else:
-        st.error('Login Failed')
+InputTools().button('Login', lambda: login(username, password))
