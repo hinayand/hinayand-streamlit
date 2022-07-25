1cbbbdbd2ebf51e34c55d23758f4b90f4f31181a
hinayand <3093812306@qq.com>
2022-07-25 17:30:13 +0800

    fix(Admin Shell):fix some bugs

--- pages/Admin Shell.py
+++ pages/Admin Shell.py
@@ -31,5 +31,4 @@ class Shell(object):
 
 PageTools().set_title('Admin Shell')
 PageTools().load_markdown_file('./markdown/admin_shell.md')
-command = InputTools().input_text_area('Command', '')
-InputTools().button('Execute', lambda: Shell().run_command(command))
+shell_object = Shell()
